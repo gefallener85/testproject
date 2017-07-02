167f9becb37ee6bddad0523361d418b1e00357e9
gefallener85 <gefallener85@gmail.com>
2017-07-02 07:30:57 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/**
+ * Created by user on 30.06.17.
+ */
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
+
+gulp.task('scss', function () {
+    return gulp.src('app/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            basedir: 'app'
+        },
+        notify: false
+    });
+});
+
+gulp.task('watch', ['browser-sync', 'scss'], function () {
+    gulp.watch('app/sass/**/*.scss', ['scss']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+});
